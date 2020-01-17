5784406a3ffb1b55b1874bccaf27cff2d5419dc8
Jeremy Willden <jeremy.willden@gmail.com>
2020-01-16 21:30:53 -0700

    added all remaining channels on the xBox clone controller, tested all 11 buttons and 4 hats

--- joystick-test.go
+++ joystick-test.go
@@ -21,9 +21,22 @@ func main() {
 
 	// get/assign channels for specific events
 	b1press := device.OnClose(1)
+	b2press := device.OnClose(2)
+	b3press := device.OnClose(3)
+	b4press := device.OnClose(4)
+	b5press := device.OnClose(5)
+	b6press := device.OnClose(6)
+	b7press := device.OnClose(7)
+	b8press := device.OnClose(8)
+	b9press := device.OnClose(9)
+	b10press := device.OnClose(10)
+	b11press := device.OnClose(11)
 	h1move := device.OnMove(1)
+	h2move := device.OnMove(2)
+	h3move := device.OnMove(3)
+	h4move := device.OnMove(4)
 
-	// start feeding OS events onto the event channels. 
+	// start feeding OS events onto the event channels.
 	go device.ParcelOutEvents()
 
 	// handle event channels
@@ -32,9 +45,38 @@ func main() {
 			select {
 			case <-b1press:
 				log.Println("button #1 pressed")
-			case h := <-h1move:
-				hpos:=h.(CoordsEvent)
-				log.Println("hat #1 moved too:", hpos.X,hpos.Y)
+			case <-b2press:
+				log.Println("button #2 pressed")
+			case <-b3press:
+				log.Println("button #3 pressed")
+			case <-b4press:
+				log.Println("button #4 pressed")
+			case <-b5press:
+				log.Println("button #5 pressed")
+			case <-b6press:
+				log.Println("button #6 pressed")
+			case <-b7press:
+				log.Println("button #7 pressed")
+			case <-b8press:
+				log.Println("button #8 pressed")
+			case <-b9press:
+				log.Println("button #9 pressed")
+			case <-b10press:
+				log.Println("button #10 pressed")
+			case <-b11press:
+				log.Println("button #11 pressed")
+			case h1 := <-h1move:
+				hpos:=h1.(CoordsEvent)
+				log.Println("hat #1 moved to:", hpos.X,hpos.Y)
+			case h2 := <-h2move:
+				hpos:=h2.(CoordsEvent)
+				log.Println("hat #2 moved to:", hpos.X,hpos.Y)
+			case h3 := <-h3move:
+				hpos:=h3.(CoordsEvent)
+				log.Println("hat #3 moved to:", hpos.X,hpos.Y)
+			case h4 := <-h4move:
+				hpos:=h4.(CoordsEvent)
+				log.Println("hat #4 moved to:", hpos.X,hpos.Y)
 			}
 		}
 	}()
