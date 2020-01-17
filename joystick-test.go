253e0b014faabdf1c640f45ed196bbc9768bdbf9
Jeremy Willden <jeremy.willden@gmail.com>
2020-01-16 20:32:14 -0700

    fixing file names

--- joystick-test.go
+++ joystick-test.go
@@ -1,31 +1,45 @@
+// log a description of events when pressing button #1 or moving hat#1. 
+// 10sec timeout.
 package main
 
-import "go.bug.st/serial.v1"
+import . "github.com/splace/joysticks"
 import "log"
+import "time"
 
-func main () {
+func main() {
+	// try connecting to specific controller.
+	// the index is system assigned, typically it increments on each new controller added.
+	// indexes remain fixed for a given controller, if/when other controller(s) are removed.
+	device := Connect(1)
 
-mode := &serial.Mode{
-	BaudRate: 9600,
-	Parity: serial.NoParity,
-	DataBits: 8,
-	StopBits: serial.OneStopBit,
-}
+	if device == nil {
+		panic("no HIDs")
+	}
 
+	// using Connect allows a device to be interrogated
+	log.Printf("HID#1:- Buttons:%d, Hats:%d\n", len(device.Buttons), len(device.HatAxes)/2)
 
-port, err := serial.Open("/dev/ttyUSB0", mode)
-if err != nil {
-	log.Fatal(err)
-}
+	// get/assign channels for specific events
+	b1press := device.OnClose(1)
+	h1move := device.OnMove(1)
 
-n, err := port.Write([]byte{0x88, 0x01, 0x04, 0x07, 0x02, 0xFF})
-_ = n
-if err != nil {
-	log.Fatal(err)
-}
-//buff := make([]byte, 100)
-for {
+	// start feeding OS events onto the event channels. 
+	go device.ParcelOutEvents()
 
-}
+	// handle event channels
+	go func(){
+		for{
+			select {
+			case <-b1press:
+				log.Println("button #1 pressed")
+			case h := <-h1move:
+				hpos:=h.(CoordsEvent)
+				log.Println("hat #1 moved too:", hpos.X,hpos.Y)
+			}
+		}
+	}()
 
+	log.Println("Timeout in 10 secs.")
+	time.Sleep(time.Second*10)
+	log.Println("Shutting down due to timeout.")
 }
