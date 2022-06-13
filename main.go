78d279660d3b45cbd0270ba9401f91ab0efc4255
Jeremy Willden <jeremy.willden@gmail.com>
2022-06-12 19:44:42 -0600

    Control working over UDP, but need to determine if response messages will be received in order to see if the camera acknowledges the UDP commands

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import "bufio"
 import "time"
 import "math"
 import "fmt"
-import "go.bug.st/serial.v1"
+//import "go.bug.st/serial.v1"
 //import "go.bug.st/serial.v1/enumerator"
 import "encoding/hex"
 import "os/signal"
@@ -15,9 +15,8 @@ import "syscall"
 import "os"
 import "net"
 
-var netAddr = "192.168.110.110"
+var netAddr = "10.2.1.146" // "192.168.110.110"
 var netPort = "1259" // "52381"
-//var camPort serial.Port
 var killSignal chan os.Signal
 
 // must install:
@@ -157,17 +156,17 @@ func main() {
 				oldtilt = 0
 				pan = 0
 				oldpan = 0
-//				sendPanTilt(camPort, 8, pan, tilt) // 8 is broadcast to all cameras
+				sendPanTilt(cameraSendChan, 8, pan, tilt) // 8 is broadcast to all cameras
 				slowPT = false
 				slowZ = false
 			case <-b6press:
 				log.Println("button #6 pressed STOPPING ZOOM/FOCUS")
 				zoom = 0
 				oldzoom = 0
-//				sendZoom(camPort, 8, zoom) // 8 is broadcast to all cameras
+				sendZoom(cameraSendChan, 8, zoom) // 8 is broadcast to all cameras
 				focus = 0
 				oldfocus = 0
-//				sendFocus(camPort, 8, focus) // 8 is broadcast to all cameras
+				sendFocus(cameraSendChan, 8, focus) // 8 is broadcast to all cameras
 				slowPT = false
 				slowZ = false
 			case <-b7press:
@@ -196,17 +195,17 @@ func main() {
 				oldtilt = 0
 				pan = 0
 				oldpan = 0
-//				sendPanTilt(camPort, 8, pan, tilt) // 8 is broadcast to all cameras
+				sendPanTilt(cameraSendChan, 8, pan, tilt) // 8 is broadcast to all cameras
 				slowPT = false
 				slowZ = false
 			case <-b6release:
 				log.Println("button #6 released STOPPING ZOOM/FOCUS")
 				zoom = 0
 				oldzoom = 0
-//				sendZoom(camPort, 8, zoom) // 8 is broadcast to all cameras
+				sendZoom(cameraSendChan, 8, zoom) // 8 is broadcast to all cameras
 				focus = 0
 				oldfocus = 0
-//				sendFocus(camPort, 8, focus) // 8 is broadcast to all cameras
+				sendFocus(cameraSendChan, 8, focus) // 8 is broadcast to all cameras
 				slowPT = false
 				slowZ = false
 			case <-b7release:
@@ -233,43 +232,43 @@ func main() {
 			// they are single-byte to avoid race issues
 			// only write them in the joystick routine
 			// read them here and watch for changes
-//			log.Println("loop ", loop1, " ", loop2 , " ", loop3, " ", loop4)
+			//log.Println("loop ", loop1, " ", loop2 , " ", loop3, " ", loop4)
 			time.Sleep(time.Millisecond*125)
 			if(oldpan != pan) {
 				oldpan = pan
 				log.Println("Pan is now:", oldpan)
-//				sendPanTilt(camPort, 8, speedLimit(pan, slowPT), speedLimit(tilt, slowPT)) // 8 is broadcast to all cameras
+				sendPanTilt(cameraSendChan, 8, speedLimit(pan, slowPT), speedLimit(tilt, slowPT)) // 8 is broadcast to all cameras
 			}
 			if(oldtilt != tilt) {
 				oldtilt = tilt
 				log.Println("Tilt is now:", oldtilt)
-//				sendPanTilt(camPort, 8, speedLimit(pan, slowPT), speedLimit(tilt, slowPT)) // 8 is broadcast to all cameras
+				sendPanTilt(cameraSendChan, 8, speedLimit(pan, slowPT), speedLimit(tilt, slowPT)) // 8 is broadcast to all cameras
 			}
 			if(oldSlowPT != slowPT) {
 				oldSlowPT = slowPT
 				log.Println("Tilt speed change")
-//				sendPanTilt(camPort, 8, speedLimit(pan, slowPT), speedLimit(tilt, slowPT)) // 8 is broadcast to all cameras
+				sendPanTilt(cameraSendChan, 8, speedLimit(pan, slowPT), speedLimit(tilt, slowPT)) // 8 is broadcast to all cameras
 			}
 			if((oldzoom != zoom) || (oldSlowZ != slowZ)) {
 				oldzoom = zoom
 				if(slowZ) {
 					log.Println("Zooming SLOWLY")
 					if(zoom>0) {
-//						sendZoom(camPort, 8, 1) // 8 is broadcast to all cameras
+						sendZoom(cameraSendChan, 8, 1) // 8 is broadcast to all cameras
 					} else if(zoom<0) {
-//						sendZoom(camPort, 8, -1) // 8 is broadcast to all cameras
+						sendZoom(cameraSendChan, 8, -1) // 8 is broadcast to all cameras
 					} else {
-//						sendZoom(camPort, 8, 0) // 8 is broadcast to all cameras
+						sendZoom(cameraSendChan, 8, 0) // 8 is broadcast to all cameras
 					}
 				} else {
 					log.Println("Zoom is now:", oldzoom)
-//					sendZoom(camPort, 8, zoom) // 8 is broadcast to all cameras
+					sendZoom(cameraSendChan, 8, zoom) // 8 is broadcast to all cameras
 				}
 			}
 			if(oldfocus != focus) {
 				oldfocus = focus
 				log.Println("Focus is now:", oldfocus)
-//				sendFocus(camPort, 8, focus) // 8 is broadcast to all cameras
+				sendFocus(cameraSendChan, 8, focus) // 8 is broadcast to all cameras
 			}
 		}
 		log.Println("exiting final for loop")
@@ -289,82 +288,82 @@ func main() {
 }
 /*
 func gotoWideShot () {
-	gotoZoom(camPort, 8, 1000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 980, 65536 - 180)
+	gotoZoom(cameraSendChan, 8, 1000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 980, 65536 - 180)
 }
 
 func gotoCloseShot () {
-	gotoZoom(camPort, 8, 12500)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 982, 65536 - 123)
+	gotoZoom(cameraSendChan, 8, 12500)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 982, 65536 - 123)
 }
 
 func gotoLeftShot () {
-	gotoZoom(camPort, 8, 8000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 1100, 65536 - 123)
+	gotoZoom(cameraSendChan, 8, 8000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 1100, 65536 - 123)
 }
 
 func gotoTempShot () { // TODO: Adjust
-	gotoZoom(camPort, 8, 14500)
-	gotoPanTilt(camPort, 8, 10, 10, 65536-97, 65536-132)
+	gotoZoom(cameraSendChan, 8, 14500)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536-97, 65536-132)
 }
 
 func gotoRightShot () {
-	gotoZoom(camPort, 8, 8000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 860, 65536 - 123)
+	gotoZoom(cameraSendChan, 8, 8000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 860, 65536 - 123)
 }
 
 func gotoPianoShot () {
-	gotoZoom(camPort, 8, 12000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 1302, 65536 - 100)
+	gotoZoom(cameraSendChan, 8, 12000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 1302, 65536 - 100)
 }
 
 func gotoDirectorShot () {
-	gotoZoom(camPort, 8, 12000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 950, 65536 - 90)
+	gotoZoom(cameraSendChan, 8, 12000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 950, 65536 - 90)
 }
 
 func gotoOrganShot () {
-	gotoZoom(camPort, 8, 12000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 915, 65536 - 100)
+	gotoZoom(cameraSendChan, 8, 12000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 915, 65536 - 100)
 }
 
 func gotoCloseLeftShot () { // TODO: Adjust
-	gotoZoom(camPort, 8, 13000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 26, 65536 - 95)
+	gotoZoom(cameraSendChan, 8, 13000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 26, 65536 - 95)
 }
 
 func gotoCloseRightShot () { // TODO: Adjust
-	gotoZoom(camPort, 8, 13000)
-	gotoPanTilt(camPort, 8, 10, 10, 14, 65536 - 95)
+	gotoZoom(cameraSendChan, 8, 13000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 14, 65536 - 95)
 }
 
 func gotoMediumShot () {
-	gotoZoom(camPort, 8, 5000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 980, 65536 - 127)
+	gotoZoom(cameraSendChan, 8, 5000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 980, 65536 - 127)
 }
 
 func gotoMediumCloseShot () { // TODO: Adjust
-	gotoZoom(camPort, 8, 10500)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 6, 65536 - 95)
+	gotoZoom(cameraSendChan, 8, 10500)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 6, 65536 - 95)
 }
 
 func gotoChoirShot () {
-	gotoZoom(camPort, 8, 7000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536 - 1060, 65536 - 100)
+	gotoZoom(cameraSendChan, 8, 7000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536 - 1060, 65536 - 100)
 }
 
 func gotoWideScreenShot () { // TODO: Adjust
-	gotoZoom(camPort, 8, 2000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536-3, 65536-80)
+	gotoZoom(cameraSendChan, 8, 2000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536-3, 65536-80)
 }
 
 func gotoScreenShot () { // TODO: Adjust
-	gotoZoom(camPort, 8, 11000)
-	gotoPanTilt(camPort, 8, 10, 10, 65536-3, 20)
+	gotoZoom(cameraSendChan, 8, 11000)
+	gotoPanTilt(cameraSendChan, 8, 10, 10, 65536-3, 20)
 }
 */
 func cameraComm(cameraSendChan <-chan []byte, cameraReceiveChan chan<- []byte, cameraErrChan chan<- bool) {
-	udpbuf := make([]byte, 2048)
+//	udpbuf := make([]byte, 2048)
 	udpconn, err := net.Dial("udp", netAddr+":"+netPort)
 	if err != nil {
 		fmt.Printf("Got an error opening the UDP port %v", err)
@@ -416,61 +415,61 @@ func cameraRead(scanner *bufio.Scanner, cameraReceiveChan chan<- []byte, cameraE
 	return
 }*/
 
-func sendZoom(port serial.Port, cam byte, zoom int8) {
+func sendZoom(cameraSendChan chan<- []byte, cam byte, zoom int8) {
 	if((zoom>0) && (zoom<=7)) {
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x07, (0x20+(byte(zoom))), 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x07, (0x20+(byte(zoom))), 0xFF})
 	} else if((zoom<0) && (zoom>=-7)) {
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x07, (0x30+(byte(0-zoom))), 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x07, (0x30+(byte(0-zoom))), 0xFF})
 	} else {
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x07, 0x00, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x07, 0x00, 0xFF})
 	}
 }
 
-func gotoZoom(port serial.Port, cam byte, zoom int16) {
+func gotoZoom(cameraSendChan chan<- []byte, cam byte, zoom int16) {
 	// Direct zoom level command from 0x0 (wide) to 0x4000 (telephoto)
 	if((zoom>=0) && (zoom<=0x4000)) {
 		p := byte(0x0F & (zoom >> 12))
 		q := byte(0x0F & (zoom >> 8))
 		r := byte(0x0F & (zoom >> 4))
 		s := byte(0x0F & zoom)
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x47, p, q, r, s, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x47, p, q, r, s, 0xFF})
 	}
 }
 
-func stopZoom(port serial.Port, cam byte) {
-	sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x07, 0x00, 0xFF})
+func stopZoom(cameraSendChan chan<- []byte, cam byte) {
+	sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x07, 0x00, 0xFF})
 }
 
-func gotoFocus(port serial.Port, cam byte, focus int16) {
+func gotoFocus(cameraSendChan chan<- []byte, cam byte, focus int16) {
 	// Direct focus level command, levels may not be specified, using the same as zoom
 	if((focus>=0) && (focus<=0x4000)) {
 		p := byte(0x0F & (focus >> 12))
 		q := byte(0x0F & (focus >> 8))
 		r := byte(0x0F & (focus >> 4))
 		s := byte(0x0F & focus)
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x48, p, q, r, s, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x48, p, q, r, s, 0xFF})
 	}
 }
 
-func stopFocus(port serial.Port, cam byte) {
-	sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x08, 0x00, 0xFF})
+func stopFocus(cameraSendChan chan<- []byte, cam byte) {
+	sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x08, 0x00, 0xFF})
 }
 
-func onePushAutoFocus(port serial.Port, cam byte) {
-	sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x18, 0x01, 0xFF})
+func onePushAutoFocus(cameraSendChan chan<- []byte, cam byte) {
+	sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x18, 0x01, 0xFF})
 }
 
-func sendFocus(port serial.Port, cam byte, focus int8) {
+func sendFocus(cameraSendChan chan<- []byte, cam byte, focus int8) {
 	if(focus>0) {
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x08, 0x02, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x08, 0x02, 0xFF})
 	} else if(focus<0) {
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x08, 0x03, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x08, 0x03, 0xFF})
 	} else {
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x08, 0x00, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x08, 0x00, 0xFF})
 	}
 }
 
-func gotoZoomFocus(port serial.Port, cam byte, zoom int16, focus int16) {
+func gotoZoomFocus(cameraSendChan chan<- []byte, cam byte, zoom int16, focus int16) {
 	// Direct zoom level command from 0x0 (wide) to 0x4000 (telephoto)
 	if((zoom>=0) && (zoom<=0x4000) && (focus>=0) && (focus<=0x4000)) {
 		p := 0x0F & byte(zoom >> 12)
@@ -481,11 +480,11 @@ func gotoZoomFocus(port serial.Port, cam byte, zoom int16, focus int16) {
 		u := 0x0F & byte(focus >> 8)
 		v := 0x0F & byte(focus >> 4)
 		w := 0x0F & byte(focus)
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x47, p, q, r, s, t, u, v, w, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x47, p, q, r, s, t, u, v, w, 0xFF})
 	}
 }
 
-func gotoPanTilt(port serial.Port, cam byte, panspeed int16, tiltspeed int16, pan uint16, tilt uint16) {
+func gotoPanTilt(cameraSendChan chan<- []byte, cam byte, panspeed int16, tiltspeed int16, pan uint16, tilt uint16) {
 	// Direct pan and tilt command at specific speed
 	var m, n byte
 	if(panspeed>24) {panspeed = 0}
@@ -502,65 +501,61 @@ func gotoPanTilt(port serial.Port, cam byte, panspeed int16, tiltspeed int16, pa
 	u := 0x0F & byte(tilt >> 8)
 	v := 0x0F & byte(tilt >> 4)
 	w := 0x0F & byte(tilt)
-	sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x02, m, n, p, q, r, s, t, u, v, w, 0xFF})
+	sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x02, m, n, p, q, r, s, t, u, v, w, 0xFF})
 }
 
-func sendPanTilt(port serial.Port, cam byte, pan int8, tilt int8) {
+func sendPanTilt(cameraSendChan chan<- []byte, cam byte, pan int8, tilt int8) {
 	if(pan>24) {pan = 0}
 	if(pan<(-24)) {pan = 0}
 	if(tilt>20) {tilt = 0}
 	if(tilt<(-20)) {tilt = 0}
 	if((pan==0) && (tilt==0)) { // Stop
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, 0x00, 0x00, 0x03, 0x03, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, 0x00, 0x00, 0x03, 0x03, 0xFF})
 	} else if((pan==0) && (tilt>0)) { // Up
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, 0x00, byte(tilt), 0x03, 0x01, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, 0x00, byte(tilt), 0x03, 0x01, 0xFF})
 	} else if((pan==0) && (tilt<0)) { // Down
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, 0x00, byte(0-tilt), 0x03, 0x02, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, 0x00, byte(0-tilt), 0x03, 0x02, 0xFF})
 	} else if((pan<0) && (tilt==0)) { // Left
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(0-pan), 0x00, 0x01, 0x03, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(0-pan), 0x00, 0x01, 0x03, 0xFF})
 	} else if((pan>0) && (tilt==0)) { // Right
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(pan), 0x00, 0x02, 0x03, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(pan), 0x00, 0x02, 0x03, 0xFF})
 	} else if((pan<0) && (tilt>0)) { // UpLeft
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(0-pan), byte(tilt), 0x01, 0x01, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(0-pan), byte(tilt), 0x01, 0x01, 0xFF})
 	} else if((pan>0) && (tilt>0)) { // UpRight
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(pan), byte(tilt), 0x02, 0x01, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(pan), byte(tilt), 0x02, 0x01, 0xFF})
 	} else if((pan<0) && (tilt<0)) { // DownLeft
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(0-pan), byte(0-tilt), 0x01, 0x02, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(0-pan), byte(0-tilt), 0x01, 0x02, 0xFF})
 	} else if((pan>0) && (tilt<0)) { // DownRight
-		sendVisca(port, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(pan), byte(0-tilt), 0x02, 0x02, 0xFF})
+		sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x06, 0x01, byte(pan), byte(0-tilt), 0x02, 0x02, 0xFF})
 	}
 }
 
-func sendWhiteBalance(port serial.Port, cam byte, wbValue WhiteBalanceT) {
+func sendWhiteBalance(cameraSendChan chan<- []byte, cam byte, wbValue WhiteBalanceT) {
 	switch wbValue {
 		case wbAuto:
-			sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x00, 0xFF})
+			sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x00, 0xFF})
 		case wbIndoor:
-			sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x01, 0xFF})
+			sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x01, 0xFF})
 		case wbOutdoor:
-			sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x02, 0xFF})
+			sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x02, 0xFF})
 		case wbOnePush:
-			sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x03, 0xFF})
+			sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x03, 0xFF})
 		case wbManual:
-			sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x05, 0xFF})
+			sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x05, 0xFF})
 		case wbOutdoorAuto:
-			sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x06, 0xFF})
+			sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x06, 0xFF})
 		case wbSodiumLampAuto:
-			sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x07, 0xFF})
+			sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x07, 0xFF})
 		case wbSodiumAuto:
-			sendVisca(port, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x08, 0xFF})
+			sendVisca(cameraSendChan, []byte{(0x80+cam), 0x01, 0x04, 0x35, 0x08, 0xFF})
 		default:
 		// unknown white balance value
 	}
 }
 
-func sendVisca(port serial.Port, message []byte) {
-	n, err := port.Write(message)
+func sendVisca(cameraSendChan chan<- []byte, message []byte) {
+	cameraSendChan <- message
 	log.Println(hex.Dump(message))
-	_ = n
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 func AnySplit(substring string) func(data []byte, atEOF bool) (advance int, token []byte, err error) {
